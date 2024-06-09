5152ca676a752c2eb8218d044da743354532c4ea
artmexbet <maydurov2004@yandex.ru>
2024-06-09 23:29:52 +0700

    add some additional info about vehicles

--- parser/main.py
+++ parser/main.py
@@ -1,4 +1,4 @@
-import time
+from pprint import pprint
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,6 +18,9 @@ class DromPage:
     def wait_many(self, selector):
         return Wait(self.driver, 50).until(EC.visibility_of_all_elements_located(selector))
 
+    def wait_one(self, selector):
+        return Wait(self.driver, 5).until(EC.visibility_of_element_located(selector))
+
     def get_cards(self):
         tmp = self.wait_many((By.CSS_SELECTOR, '.css-4zflqt.e1huvdhj1'))
         cards_info = []
@@ -33,15 +36,49 @@ class DromPage:
                 'engine_power': int(engine_power.strip(' л.с.)')),
                 'engine_capacity': float(engine_capacity.rstrip(" л")),
                 'fuel_type': car_specs[1].text.rstrip(","),
-                'transmission': car_specs[2].text.rstrip(", "),
                 'wd': car_specs[3].text.rstrip(","),
                 'price': int(card.find_element(By.CSS_SELECTOR, 'span[data-ftid="bull_price"]').text.replace(" ", "")),
                 'year': int(year)
             }
+
             if len(car_specs) == 5:
                 card_info['mileage'] = int(car_specs[4].text.rstrip(" км").replace(" ", ""))
             else:
                 card_info['mileage'] = 0
+
+            self.driver.execute_script("arguments[0].click();", card)
+            # Получаем дополнительные данные (АКПП может делиться на несколько типов и т.д.)
+            ad_car_specs_table = (self.wait_one((By.CSS_SELECTOR, '.css-xalqz7.eppj3wm0')).
+                                  find_element(By.CSS_SELECTOR, 'tbody'))
+            ad_car_specs_rows = ad_car_specs_table.find_elements(By.CSS_SELECTOR, 'tr')
+            ad_car_specs = {}
+            for row in ad_car_specs_rows:
+                try:
+                    ad_car_specs[row.find_element(By.CSS_SELECTOR, 'th').text] = row.find_element(
+                        By.CSS_SELECTOR, 'td'
+                    ).text
+                except Exception:
+                    pass
+            card_info['transmission'] = ad_car_specs.get("Коробка передач", "-")
+            card_info['body'] = ad_car_specs.get("Тип кузова", "-")
+            card_info['color'] = ad_car_specs.get("Цвет", "-")
+            card_info['steering_wheel_pos'] = ad_car_specs.get("Руль", "-")
+            card_info['generation'] = ad_car_specs.get("Поколение", "-")
+
+            vin_specs = self.wait_many((By.CSS_SELECTOR, '.css-z05wok.eawu4md2'))
+            if len(vin_specs) > 2:
+                first = vin_specs[:2]
+                card_info['documents_cond'] = first[0].find_element(By.CSS_SELECTOR, 'button').text
+                card_info['drivers_count'] = int(first[1].find_element(By.CSS_SELECTOR, 'button').text.split()[0])
+                card_info['was_driven_by_legal_person'] = vin_specs[3].text != "Был во владении у юр. лица"
+                card_info['is_under_credit'] = vin_specs[6].text == "Ограничений не обнаружено"
+            else:
+                card_info['documents_cond'] = "-"
+                card_info['drivers_count'] = -1
+                card_info['was_driven_by_legal_person'] = False
+                card_info['is_under_credit'] = False
+            self.driver.back()
+
             cards_info.append(card_info)
         return cards_info
 
@@ -50,5 +87,9 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()
     page = DromPage(driver)
     page.open()
-    print(page.get_cards())
-    time.sleep(100)
+    res = []
+    for i in range(2, 3):
+        res += page.get_cards()
+        page.open(f'all/page{i}')
+    pprint(res[:5])
+    print(len(res))
